5faf7863eefc75deff384ea45bf0542ceafc2c9e
Chaitanya Prabhu <chaitanyasprabhu@gmail.com>
2026-02-05 22:52:59 +0530

    Implemented modular exponentiation, gcd, inverse modulus

--- crypto_utils.cpp
+++ crypto_utils.cpp
@@ -1,6 +1,8 @@
 #include "crypto_utils.h"
-#include <vector>
+#include <cstdlib>
 #include <iostream>
+#include <vector>
+#include <math.h>
 
 #define ll long long int
 
@@ -26,3 +28,47 @@ ll  mod_exp(ll  b,  ll e, ll p) {
 
   return result;
 }
+
+// euclidean algorithm
+// gcd(a, b) = gcd(a mod b, b) recursively
+
+
+
+// extended euclidean algorithm(EEA)
+// 56x + 15y = gcd(56, 15)
+// 56 = 15.3 + 11
+// 15 = 11.1 + 4
+// 11 = 4.2 + 3
+// 4 = 3.1 + 1
+// 3 = 1.3 + 0
+// gcd(56, 15) = 1
+
+ ll gcd(ll a, ll b, int &x, int &y){
+  if (b == 0) {
+    x = 1;
+    y = 0;
+
+    return a;
+  }
+
+  int x1, y1;
+  int d = gcd(b, a % b, x1, y1);
+  x = y1;
+  y = x1 - y1 * (a / b);
+
+  return d;
+}
+
+
+
+ll inv_mod(ll a, ll b) {
+  int x, y;
+  int g = gcd(a, b, x, y);
+
+  if (g != 1) {
+    return -1ll;
+  }else {
+    return (x%b + b)%b;
+  }
+}
+
--- crypto_utils.h
+++ crypto_utils.h
@@ -4,5 +4,6 @@
 #define ll long long int
 
 ll mod_exp(ll b, ll e, ll p); // (b)^e mod(p)
-ll inv_mod(ll b, ll e, ll p); // (b^-e)mod(p)
+ll inv_mod(ll a, ll m); // (b^-e)mod(p)
+ll gcd(ll a, ll b, int &x, int &y);           // gcd(a, b)
 #endif
