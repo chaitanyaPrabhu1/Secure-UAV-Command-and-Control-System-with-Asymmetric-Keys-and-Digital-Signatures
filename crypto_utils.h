eaac5c2362737a7ed4a65ec6b4986707fefb55de
Chaitanya Prabhu <97834863+chaitanyaPrabhu1@users.noreply.github.com>
2026-03-23 20:03:26 +0530

    Delete crypto_utils.h

--- crypto_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRYPTO_UTILS_H
-#define CRYPTO_UTILS_H
-
-#define ll long long int
-
-ll mod_exp(ll b, ll e, ll p); // (b)^e mod(p)
-ll inv_mod(ll a, ll m); // (b^-e)mod(p)
-ll gcd(ll a, ll b, int &x, int &y);           // gcd(a, b)
-#endif
